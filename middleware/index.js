7c2ae1788b9498902d6239ca75fc45754112bdac
LAPTOP-DHA0R3LE\Niel <niel.zebua8@gmail.com>
2022-01-26 23:44:49 +0700

    jwt dan verifikasi sudah dibuat

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var auth = require('./auth');
+var verifikasi = require('./verifikasi');
 var router = express.Router();
+
 
 //daftarkan menu registrasi 
 router.post('/api/v1/register', auth.registrasi);
 //daftarkan menu login
 router.post('/api/v1/login', auth.login);
+//alamat yang perlu otorisasi
+router.get('/api/v1/rahasia', verifikasi(2), auth.halamanrahasia);
 module.exports = router;
---
+++ middleware/verifikasi.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/secret');
+
+function verifikasi(roles){
+    return function(req,rest,next){
+        //cek authorization header
+        var tokenWithBearer = req.headers.authorization;
+        if(tokenWithBearer){
+            var token = tokenWithBearer.split(' ')[1];
+            //verifikasi
+            jwt.verify(token, config.secret, function(err, decoded){
+                if(err){
+                   return rest.status(401).send({auth:false, message:'Token tidak terdaftar!'});
+                }else{
+                    if(roles==2){
+                        req.auth = decoded;
+                        next();
+                    }else{
+                        return rest.status(401).send({auth:false, message:'Gagal mengotorisasi role anda'}); 
+                    }
+                }
+            });
+        }else{
+            return rest.status(401).send({auth:false, message:'Token tidak tersedia!'});
+        }
+    }
+}
+
+module.exports = verifikasi;
